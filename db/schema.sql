31ad72fb38364801687d7b886429d299e9fd27c2
Snack Man <lpardo1989@gmail.com>
2023-07-10 14:45:26 -0700

    schema outlined

---
+++ db/schema.sql
@@ -0,0 +1,36 @@
+//create 3 tables for departments, roles, and employees
+
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL;
+    last_name VARCHAR(100) NOT NULL;
+    role_it int NOT NULL,
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE employee_role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (10, 0) NOT NULL,
+    department_id int NOT NULL,
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id),
+);
+
+
+SELECT Math FROM department;
+
+
+
+
--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const inquirer = require('inquierer');
 const mysql = require('mysql2');
 
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password
+    password: '',
+    database: 'books_db'
+  },
+  console.log(`Connected to the books_db database.`)
+);
