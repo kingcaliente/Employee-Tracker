fa83fd6fb548505e384355b33bf4ead6b39d362d
Snack Man <lpardo1989@gmail.com>
2023-07-13 20:39:40 -0700

    schema update

--- db/schema.sql
+++ db/schema.sql
@@ -8,13 +8,13 @@ USE business;
 CREATE TABLE department(
     id INT AUTO_INCREMENT,
     roles_id INT,
-    department_name VARCHAR(25) NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE roles(
     id INT AUTO_INCREMENT,
-    title VARCHAR(25),
+    title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
     PRIMARY KEY (id),
@@ -24,8 +24,8 @@ CREATE TABLE roles(
 
 CREATE TABLE employees(
 id INT AUTO_INCREMENT,
-first_name VARCHAR(25),
-last_name VARCHAR(25),
+first_name VARCHAR(30),
+last_name VARCHAR(30),
 roles_id INT,
 manager_id INT,
 PRIMARY KEY(id),
